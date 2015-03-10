cfb8db9e1b869a65f182a9eab7bff2ca84dabd85
Onyx <M3TIOR@users.noreply.github.com>
2015-03-10 10:32:48 -0700

    testing if I can make the kernel in pure gcc C

---
+++ core/boot.c
@@ -0,0 +1,31 @@
+/* M3TIOR 2015 */
+
+
+// I know this looks bad now, but trust me this style of writing asm statements helps alot when looking for stuff...
+main(){
+	asm volatile (
+		//assembly template
+		"
+			
+		"
+		:
+		
+		//ouput operands
+		"
+			
+		"
+		:
+		
+		//input operands
+		"
+			
+		"
+		:
+		
+		//clobbers
+		"
+			
+		"
+		
+	)
+}
