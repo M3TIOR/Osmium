102333f7383e2b9acc1fbbc82e609da896ce1118
Onyx <M3TIOR@users.noreply.github.com>
2015-03-11 11:52:33 -0700

    Create mbr.asm
    This will need some modification still to actually boot the kernel for testing, but it's done thus far with comments. So I'm happy about that. Furthermore I would like to make a personal note to RETURN TO THIS!

---
+++ mbr.asm
@@ -0,0 +1,21 @@
+;Temporary file for making the kernel bootable until I can make a bootloader
+;Or get an installer up and running.
+
+[BITS 16]	;force 16 bit machiene code output
+[ORG  0x7c00]	;this defines where the program we are executing is loaded into memory.
+
+jmp boot	; no Idea why this is here particularly...
+
+boot:			;displays ! to the screen.
+	mov al, '!'
+	mov ah, 0x0e
+	mov bh, 0x00
+	mov bl, 0x07
+
+	int 0x10
+	jmp $
+
+times 510-($-$$) db 0
+
+db 0x55		;Tells the bios that this device is bootable.
+db 0xaa		;In conjunction with prevoius db.
