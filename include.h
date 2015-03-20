88da707f5ce162977c60c4143cbcf872478c7c3a
Onix18 <jtimmerman32@gmail.com>
2015-03-19 23:20:33 -0700

    updated readme...

---
+++ include.h
@@ -0,0 +1,17 @@
+// >> M3TIOR Stamp of approval and recognition <<
+
+// Everything that makes a programmers life easier;
+
+#define TRUE 1
+#define FALSE 0
+#define NULL (0)
+
+typedef unsigned char byte;
+
+//really standard stuff that >>other<< programmers find usefull...
+typedef unsigned char	 u8int;
+typedef unsigned short	 u16int;
+typedef unsigned int	 u32int;
+typedef unsigned long	 u16int;
+
+typedef char* string;
